1cdc033e0d207086bdb70e4d06889d34f4ab29ed
Enrique Benitez <enriquebenitezjr@gmail.com>
2019-09-23 16:51:36 -0500

    chore(cli): Pass args to show version and help

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,27 @@ import { Command, flags } from '@oclif/command'
 import * as prompts from './prompts'
 
 class CreateEvaApp extends Command {
-  static description = 'describe the command here'
+  static description = 'Set up an Eva-Ready web or mobile app by running one command 💖'
 
   static flags = {
     help: flags.help({ char: 'h' }),
-    version: flags.version({ char: 'v' }),
-    force: flags.boolean({ char: 'f' })
+    version: flags.version({ char: 'v' })
   }
 
   async run() {
-    this.log(`\nWelcome to CEA (create-eva-app) 😎\n\n`)
+    const {
+      // eslint-disable-next-line
+      flags: { help, version }
+    } = this.parse(CreateEvaApp)
+
+    this.log(`\nWelcome to CEA (create-eva-app) 💖\n\n`)
 
     try {
       const { name } = await prompts.name()
       await prompts.clone(name)
       this.log(`\nDone! 💖😄 Thx for using CEA, see ya later 👋\n\n`)
     } catch (err) {
-      this.log(`\nError! 😬 ${err}`)
+      this.log(`\nError! 💣 ${err}\n\n`)
     }
   }
 }
